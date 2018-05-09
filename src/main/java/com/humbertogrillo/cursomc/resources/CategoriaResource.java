8c5ca94c849ffdca9639b8b711e74e909a5bae7b
Humberto Grillo <humberto.celiasilva@gmail.com>
2018-05-09 13:49:57 -0300

    Adiciona configuração banco de dados e classe Categoria

--- src/main/java/com/humbertogrillo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/humbertogrillo/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,21 @@
 package com.humbertogrillo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.humbertogrillo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		List<Categoria> resultado = new ArrayList<>();
+		return resultado;
 	}
 }
