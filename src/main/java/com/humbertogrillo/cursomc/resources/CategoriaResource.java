5e922884bfa6fb3f2d79ec917f3ce3778edae17a
Humberto Grillo <humberto.celiasilva@gmail.com>
2018-05-09 14:18:21 -0300

    Adiciona repository e service pra Categoria

--- src/main/java/com/humbertogrillo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/humbertogrillo/cursomc/resources/CategoriaResource.java
@@ -1,21 +1,26 @@
 package com.humbertogrillo.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.humbertogrillo.cursomc.domain.Categoria;
+import com.humbertogrillo.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		List<Categoria> resultado = new ArrayList<>();
-		return resultado;
+
+	@Autowired
+	CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+
+		return ResponseEntity.ok(obj);
 	}
 }
---
+++ src/main/java/com/humbertogrillo/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.humbertogrillo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.humbertogrillo.cursomc.domain.Categoria;
+import com.humbertogrillo.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	public CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
